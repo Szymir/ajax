c7f4fb7fbc080d0c399a6c2eb51b94a6ce538e8e
Szymon Malewski <malewskiszymon@gmail.com>
2018-03-10 10:48:49 +0100

    Add all

--- js/script.js
+++ js/script.js
@@ -1,35 +1,21 @@
-$(function() {
-	var tweetLink = "https://twitter.com/intent/tweet?text=";
-	var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-	var prefix = "https://cors-anywhere.herokuapp.com/";
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
 
-	function getQuote() {
-	    $.getJSON(prefix + quoteUrl, createTweet);
-	    $.ajaxSetup({ cache: false });
-	}
+$('#search').click(searchCountries);
 
-	function createTweet(input) {
-	    var data = input[0];
-	    var quoteText = $(data.content).text().trim();
-	    var quoteAuthor = data.title;
-	    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+function searchCountries() {
+ 	var countryName = $('#country-name').val();
+if(!countryName.length) countryName = 'Poland';
+$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
 
-	    if (!quoteAuthor.length) {
-	        quoteAuthor = "Unknown author";
-	    }
-
-	    if (tweetText.length > 140) {
-		    getQuote();
-		} else {
-		    var tweet = tweetLink + encodeURIComponent(tweetText);
-		    $('.quote').text(quoteText);
-		    $('.author').text("Author: " + quoteAuthor);
-		    $('.tweet').attr('href', tweet);
-		}
-	}
-
-	// getQuote();
-    $('.trigger').click(function() {
-        getQuote();
+function showCountriesList(resp) {
+    countriesList.empty();
+    resp.forEach(function(item) {
+    	$('<li>').text(item.name).appendTo(countriesList);
     });
-});
+}
